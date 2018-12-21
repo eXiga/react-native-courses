01029cf61f171869d255af348aa443cdbce0eacc
eXiga <anton.kostenich@hotmail.com>
2018-12-21 17:38:19 +0100

    RNM-3: Layout and style advanced
      - update product model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,15 +1,24 @@
+interface ICoordinate {
+  latitude: number;
+  longitude: number;
+}
+
 class Product {
   id: number;
   name: string;
   description: string;
   imagePath: any;
+  location: ICoordinate;
+  phone: string;
 
-  constructor(id: number, name: string, description: string, imagePath: any) {
+  constructor(id: number, name: string, description: string, imagePath: any, location: ICoordinate, phone: string) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.location = location;
+    this.phone = phone;
   }
 }
 
-export { Product }
+export { Product };
